324f6cfc7a84e479e6128994273a6fd1b6d182e2
Pedro Costa <dev@iampfac.com>
2013-07-09 19:21:31 +0100

    Added Armadillo and matrix loading.

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,38 @@
 // project headers
 #include "main_options.h"
 #include <msc/cuda>
+#include <msc/matrix/print>
 
 // stc C++ headers
 #include <iostream>
+
+// Armadillo headers
+#include <armadillo>
 
 
 // names
 using std::clog;
 
+using arma::Mat;
+
 
 // macros
 #define endl '\n'
+
+
+template<typename T>
+int _main () {
+	Mat<T> arma_matrix;
+	if (!arma_matrix.load(filename))
+		return 1;
+
+	const ulong m = arma_matrix.n_rows;
+
+	if (print_sqrtm)
+		print(arma_matrix);
+
+	return 0;
+}
 
 
 int main (int argc, char * argv[]) {
@@ -26,10 +47,8 @@ int main (int argc, char * argv[]) {
 	#endif
 
 	CUDA::query_devices(true);
-	if (CUDA::get_device(0).supports_double_precision()) {
-		clog << "Running with double precision" << endl;
-	} else
-		clog << "Running with single precision" << endl;
-
-	return 0;
+	if (CUDA::get_device(0).supports_double_precision())
+		return _main<double>();
+	else
+		return _main<float>();
 }
