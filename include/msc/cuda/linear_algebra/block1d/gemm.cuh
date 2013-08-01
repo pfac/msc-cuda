bda1aa6d69f0994c385d49c353e60c3d027aeffd
Pedro Costa <dev@iampfac.com>
2013-08-01 22:40:37 +0100

    Fixes bug in GEMM

--- include/msc/cuda/linear_algebra/block1d/gemm.cuh
+++ include/msc/cuda/linear_algebra/block1d/gemm.cuh
@@ -5,16 +5,19 @@
 namespace CUDA { namespace linear_algebra { namespace block1D {
 
 
+	/* C = alpha * A * B + beta * C */
 	template<typename T>
 	__device__
 	void gemm (const ulong m, const ulong n, const ulong l, const T alpha, const T * const a, const T * const b, const T beta, T * const c) {
 		for (ulong j = 0; j < n; ++j) {
-			for (ulong i = threadIdx.x; i < m; i += blockDim.x)
-				c[j * n + i] *= beta;
+			for (ulong i = threadIdx.x; i < m; i += blockDim.x) {
+				c[j * m + i] *= beta;
+			}
 
 			for (ulong k = 0; k < l; ++k) {
-				for (ulong i = threadIdx.x; i < m; i += blockDim.x)
-					c[j * n + i] += alpha * a[k * l + i] * b[j * n + k];
+				for (ulong i = threadIdx.x; i < m; i += blockDim.x) {
+					c[j * m + i] += alpha * a[k * m + i] * b[j * l + k];
+				}
 			}
 		}
 	}
--- test/cuda/linear_algebra/block1d/gemm.cu
+++ test/cuda/linear_algebra/block1d/gemm.cu
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "../../../../vendor/nvidia/cuda/cuPrintf.cu"
+// #include "../../../../vendor/nvidia/cuda/cuPrintf.cu"
 // project headers
 #include <msc/cuda/array>
 #include <msc/cuda/linear_algebra/block1d/gemm>
@@ -45,11 +45,67 @@ TEST(SmallFloatTestCase, GEMMtest) {
 	CUDA::array<float> d_a(a, SMALL_DIMM);
 	CUDA::array<float> d_c(c, SMALL_DIMM);
 
+	// cudaPrintfInit();
+
 	test_gemm<<< 1 , SMALL_DIMV >>>(SMALL_DIMV, SMALL_DIMV, SMALL_DIMV, alpha, d_a.get_pointer(), d_a.get_pointer(), beta, d_c.get_pointer());
 	HANDLE_LAST_ERROR();
 
 	d_c.to_host(c);
 
+	// cudaDeviceSynchronize();
+	// cudaPrintfDisplay(stdout, true);
+	// cudaPrintfEnd();
+
 	for (ulong i = 0; i < SMALL_DIMM; ++i)
 		EXPECT_FLOAT_EQ(c[i], s[i]);
+}
+
+
+TEST(Float7x7x2case01, GEMMtest) {
+	const ulong l = 7;
+	const ulong m = 7;
+	const ulong n = 2;
+
+	const float a[l * m] = {
+		29, 30, 31, 32, 33, 34, 35,
+		37, 38, 39, 40, 41, 42, 43,
+		46, 47, 48, 49, 50, 51, 52,
+		56, 57, 58, 59, 60, 61, 62,
+		67, 68, 69, 70, 71, 72, 73,
+		79, 80, 81, 82, 83, 84, 85,
+		92, 93, 94, 95, 96, 97, 98
+	};
+	const float b[m * n] = {
+		414, 415, 416, 417, 418, 419, 420,
+		443, 444, 445, 446, 447, 448, 449
+	};
+	float c[l * n] = {
+		1750904, 1762299, 1772469, 1781000, 1787475, 1791474, 1792574,
+		2072225, 2084868, 2096199, 2105775, 2113150, 2117875, 2119498
+	};
+	const float s[l * n] = {
+		1581308, 1589784, 1597035, 1602647, 1606203, 1607283, 1605464,
+		1890855, 1900376, 1908585, 1915039, 1919292, 1920895, 1919396
+	};
+
+	const float alpha = -1.0;
+	const float beta = 1.0;
+
+	// cudaPrintfInit();
+
+	CUDA::array<float> d_a(a, l * m);
+	CUDA::array<float> d_b(b, m * n);
+	CUDA::array<float> d_c(c, l * n);
+
+	test_gemm<<< 1 , l >>>(l, n, m, alpha, d_a.get_pointer(), d_b.get_pointer(), beta, d_c.get_pointer());
+	HANDLE_LAST_ERROR();
+
+	d_c.to_host(c);
+
+	// cudaDeviceSynchronize();
+	// cudaPrintfDisplay(stdout, true);
+	// cudaPrintfEnd();
+
+	for (ulong i = 0; i < l * n; ++i)
+		EXPECT_FLOAT_EQ(s[i], c[i]);
 }
